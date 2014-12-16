8cd0a81f20ed2e6005e7d3aa5a3f343195abf69b
Dov Frankel <dov@abbey-code.com>
2014-12-16 13:45:01 -0500

    Added initializers and properties to UZKArchive

--- Source/UZKArchive.h
+++ Source/UZKArchive.h
@@ -8,4 +8,50 @@
 
 @interface UZKArchive : NSObject
 
+/**
+ *  The URL of the archive
+ */
+@property(weak, readonly) NSURL *fileURL;
+
+/**
+ *  The filename of the archive
+ */
+@property(weak, readonly) NSString *filename;
+
+/**
+ *  The password of the archive
+ */
+@property(strong, strong) NSString *password;
+
+
+/**
+ *  Creates and returns an archive at the given path
+ *
+ *  @param filePath A path to the archive file
+ */
++ (instancetype)zipArchiveAtPath:(NSString *)filePath;
+
+/**
+ *  Creates and returns an archive at the given URL
+ *
+ *  @param fileURL The URL of the archive file
+ */
++ (instancetype)zipArchiveAtURL:(NSURL *)fileURL;
+
+/**
+ *  Creates and returns an archive at the given path, with a given password
+ *
+ *  @param filePath A path to the archive file
+ *  @param password The passowrd of the given archive
+ */
++ (instancetype)zipArchiveAtPath:(NSString *)filePath password:(NSString *)password;
+
+/**
+ *  Creates and returns an archive at the given URL, with a given password
+ *
+ *  @param fileURL  The URL of the archive file
+ *  @param password The passowrd of the given archive
+ */
++ (instancetype)zipArchiveAtURL:(NSURL *)fileURL password:(NSString *)password;
+
 @end
--- Source/UZKArchive.m
+++ Source/UZKArchive.m
@@ -6,6 +6,118 @@
 
 #import "UZKArchive.h"
 
+
+@interface UZKArchive ()
+
+@property (strong) NSData *fileBookmark;
+
+@end
+
+
 @implementation UZKArchive
+
+
+
+#pragma mark - Convenience Methods
+
+
++ (UZKArchive *)zipArchiveAtPath:(NSString *)filePath
+{
+    return [[UZKArchive alloc] initWithFile:[NSURL fileURLWithPath:filePath]];
+}
+
++ (UZKArchive *)zipArchiveAtURL:(NSURL *)fileURL
+{
+    return [[UZKArchive alloc] initWithFile:fileURL];
+}
+
++ (UZKArchive *)zipArchiveAtPath:(NSString *)filePath password:(NSString *)password
+{
+    return [[UZKArchive alloc] initWithFile:[NSURL fileURLWithPath:filePath]
+                                   password:password];
+}
+
++ (UZKArchive *)zipArchiveAtURL:(NSURL *)fileURL password:(NSString *)password
+{
+    return [[UZKArchive alloc] initWithFile:fileURL password:password];
+}
+
+
+
+#pragma mark - Initializers
+
+
+- (id)initWithFile:(NSURL *)fileURL
+{
+    if ((self = [super init])) {
+        NSError *error = nil;
+        self.fileBookmark = [fileURL bookmarkDataWithOptions:0
+                              includingResourceValuesForKeys:@[]
+                                               relativeToURL:nil
+                                                       error:&error];
+        
+        if (error) {
+            NSLog(@"Error creating bookmark to ZIP archive: %@", error);
+        }
+    }
+    
+    return self;
+}
+
+- (id)initWithFile:(NSURL *)fileURL password:(NSString*)password
+{
+    if ((self = [self initWithFile:fileURL])) {
+        self.password = password;
+    }
+    
+    return self;
+}
+
+
+
+#pragma mark - Properties
+
+
+- (NSURL *)fileURL
+{
+    BOOL bookmarkIsStale = NO;
+    NSError *error = nil;
+    
+    NSURL *result = [NSURL URLByResolvingBookmarkData:self.fileBookmark
+                                              options:0
+                                        relativeToURL:nil
+                                  bookmarkDataIsStale:&bookmarkIsStale
+                                                error:&error];
+    
+    if (error) {
+        NSLog(@"Error resolving bookmark to ZIP archive: %@", error);
+        return nil;
+    }
+    
+    if (bookmarkIsStale) {
+        self.fileBookmark = [result bookmarkDataWithOptions:0
+                             includingResourceValuesForKeys:@[]
+                                              relativeToURL:nil
+                                                      error:&error];
+        
+        if (error) {
+            NSLog(@"Error creating fresh bookmark to ZIP archive: %@", error);
+        }
+    }
+    
+    return result;
+}
+
+- (NSString *)filename
+{
+    NSURL *url = self.fileURL;
+    
+    if (!url) {
+        return nil;
+    }
+    
+    return url.path;
+}
+
 
 @end
