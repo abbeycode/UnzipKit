b7bd780b300cc5e9ce2bf3a4211d5dba73de76b1
Dov Frankel <dov@abbey-code.com>
2015-04-08 10:36:38 -0400

    Added initial iOS project

---
+++ UnzipKitDemo/UnzipKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnzipKitDemo
+//
+//  Created by Dov Frankel on 4/8/15.
+//  Copyright (c) 2015 Abbey Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
