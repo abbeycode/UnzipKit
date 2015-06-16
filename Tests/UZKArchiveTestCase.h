2e65080dcb0e6be368c85a62d1e5d08acba05941
Dov Frankel <dov@abbey-code.com>
2015-06-16 13:10:55 -0400

    Added test cases (including a Swift one) to create password-protected archives (Issue #12)

---
+++ Tests/UZKArchiveTestCase.h
@@ -0,0 +1,43 @@
+//
+//  UZKArchiveTestCase.h
+//  UnzipKit
+//
+//  Created by Dov Frankel on 6/16/15.
+//  Copyright (c) 2015 Abbey Code. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+
+@interface UZKArchiveTestCase : XCTestCase
+
+@property BOOL testFailed;
+
+@property NSURL *tempDirectory;
+@property NSMutableDictionary *testFileURLs;
+@property NSMutableDictionary *unicodeFileURLs;
+@property NSSet *nonZipTestFilePaths;
+@property NSSet *nonZipUnicodeFilePaths;
+@property NSURL *corruptArchive;
+
+
+// Helper Methods
+
++ (NSDateFormatter *)dateFormatter;
+
+- (NSURL *)urlOfTestFile:(NSString *)filename;
+
+- (NSString *)randomDirectoryName;
+- (NSString *)randomDirectoryWithPrefix:(NSString *)prefix;
+
+- (NSInteger)numberOfOpenFileHandles;
+
+- (NSURL *)emptyTextFileOfLength:(NSUInteger)fileSize;
+- (NSURL *)archiveWithFiles:(NSArray *)fileURLs;
+- (NSURL *)largeArchive;
+
+- (NSUInteger)crcOfTestFile:(NSString *)filename;
+
+@end
+
