557e86be6c963eb02fbc5e1e37a57e44d9bf57f0
Dov Frankel <dov@abbey-code.com>
2015-06-18 15:08:23 -0400

    Updated Carthage validation script to be more friendly for local development, and moved that and the CocoaPods validation script to the Script section, so they will fail the build if broken

--- Scripts/carthage-validate.sh
+++ Scripts/carthage-validate.sh
@@ -1,5 +1,14 @@
+if [ -z ${TRAVIS+x} ]; then
+    TRAVIS_BUILD_DIR="~/Source Code/UnzipKit"
+    TRAVIS_BRANCH=carthage
+fi
+
+rm UnzipKitDemo/Cartfile
+rm UnzipKitDemo/Cartfile.resolved
+rm -rf UnzipKitDemo/Carthage
+
 echo "git \"$TRAVIS_BUILD_DIR\" \"$TRAVIS_BRANCH\"" > UnzipKitDemo/Cartfile
 
 pushd UnzipKitDemo
-carthage update --configuration Debug --verbose
+carthage bootstrap --configuration Debug --verbose
 popd
